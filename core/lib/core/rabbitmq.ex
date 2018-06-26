32b2f585e62ddc196efa94341886552228d4a990
Thomas Gatzweiler <mail@thomasgatzweiler.com>
2018-06-26 20:56:24 +0200

    Use RabbitMQ federation

--- core/lib/core/rabbitmq.ex
+++ core/lib/core/rabbitmq.ex
@@ -7,7 +7,7 @@ defmodule Core.RabbitMQ do
     GenServer.start_link(__MODULE__, [], [])
   end
 
-  @exchanges ["calls", "telemetry"]
+  @exchanges ["dapnet.calls", "dapnet.telemetry"]
 
   def init(_opts) do
     Process.send_after(self(), :connect, 5000)
@@ -26,7 +26,7 @@ defmodule Core.RabbitMQ do
           :ok = Exchange.fanout(chan, exchange, durable: true)
         end)
 
-        Process.send_after(self(), :shovel, 5000)
+        Process.send_after(self(), :federation, 5000)
 
         {:noreply, chan}
       _ ->
@@ -36,49 +36,74 @@ defmodule Core.RabbitMQ do
     end
   end
 
-  def handle_info(:shovel, state) do
+  def handle_info(:federation, state) do
     Core.Discovery.reachable_nodes() |> Enum.each(fn {node, _} ->
-      Enum.each(@exchanges, fn exchange ->
-        case shovel_get(node, exchange) do
-          {:ok, %HTTPoison.Response{status_code: 200}} ->
-            Logger.debug("Shovel #{node}-#{exchange} exists.")
-          {:ok, %HTTPoison.Response{status_code: 404}} ->
-            case shovel_create(node, exchange) do
-              {:ok, _} -> Logger.info("Creating shovel #{node}-#{exchange}.")
-              _ -> Logger.error("Failed to create shovel #{node}-#{exchange}.")
-            end
-          {:error, %HTTPoison.Error{reason: reason}} ->
-            Logger.error("Failed to query shovel #{node}-#{exchange}.")
-        end
-      end)
+      case federation_get(node) do
+        {:ok, %HTTPoison.Response{status_code: 200}} ->
+          Logger.debug("Federation with #{node} exists.")
+        {:ok, %HTTPoison.Response{status_code: 404}} ->
+          case federation_create(node) do
+            {:ok, _} -> Logger.info("Creating federation to #{node}.")
+            _ -> Logger.error("Failed to create federation to #{node}.")
+          end
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          Logger.error("Failed to query federation #{node}.")
+      end
     end)
 
-    Process.send_after(self(), :shovel, 60000)
+    case policy_get() do
+      {:ok, %HTTPoison.Response{status_code: 200}} ->
+        Logger.debug("DAPNET federation policy exists.")
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        case policy_create() do
+          {:ok, _} -> Logger.info("Creating DAPNET federation policy.")
+          _ -> Logger.error("Failed to create DAPNET federation policy.")
+        end
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        Logger.error("Failed to query DAPNET federation policy.")
+    end
+
+    Process.send_after(self(), :federation, 60000)
     {:noreply, state}
   end
 
-  def shovel_create(node, exchange) do
-    url = "http://rabbitmq:15672/api/parameters/shovel/%2f/#{node}-#{exchange}"
-    params = %{value: %{"src-protocol": "amqp091",
-                        "src-uri":  "amqp://",
-                        "src-exchange":  exchange,
-                        "dest-protocol": "amqp091",
-                        "dest-uri": "amqp://guest:guest@#{node}",
-                        "dest-exchange": exchange
+  def federation_create(node) do
+    url = "http://rabbitmq:15672/api/parameters/federation-upstream/%2f/#{node}"
+    params = %{value: %{"uri": "amqp://guest:guest@#{node}",
+                        "expires": 3600000,
+                        "max-hops": 3
                        }} |> Poison.encode!
 
     options = [hackney: [basic_auth: {"guest", "guest"}]]
     HTTPoison.put(url, params, [], options)
   end
 
-  def shovel_delete(node, exchange) do
-    url = "http://rabbitmq:15672/api/parameters/shovel/%2f/#{node}-#{exchange}"
+  def federation_delete(node) do
+    url = "http://rabbitmq:15672/api/parameters/federation-upstream/%2f/#{node}"
     options = [hackney: [basic_auth: {"guest", "guest"}]]
     HTTPoison.delete(url, [], options)
   end
 
-  def shovel_get(node, exchange) do
-    url = "http://rabbitmq:15672/api/parameters/shovel/%2f/#{node}-#{exchange}"
+  def federation_get(node) do
+    url = "http://rabbitmq:15672/api/parameters/federation-upstream/%2f/#{node}"
+    options = [hackney: [basic_auth: {"guest", "guest"}]]
+    HTTPoison.get(url, [], options)
+  end
+
+  def policy_create() do
+    url = "http://rabbitmq:15672/api/policies/%2f/dapnet-federation"
+    params = %{"pattern": "^dapnet\.",
+               "definition": %{"federation-upstream-set": "all"},
+               "apply-to": "exchanges"
+              } |> Poison.encode!
+
+    options = [hackney: [basic_auth: {"guest", "guest"}]]
+
+    HTTPoison.put(url, params, [], options)
+  end
+
+  def policy_get() do
+    url = "http://rabbitmq:15672/api/policies/%2f/dapnet-federation"
     options = [hackney: [basic_auth: {"guest", "guest"}]]
     HTTPoison.get(url, [], options)
   end
